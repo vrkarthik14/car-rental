87537c250d4da7360764aebad11d44e649915fe4
Karthik Vr <karthik.vr@grabtaxi.com>
2021-12-27 19:32:37 +0530

    [NOJIRA-0][INITIAL_COMMIT] initial commit

---
+++ cloud-gw/src/test/java/com/codejam/cloudgw/CloudGwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codejam.cloudgw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
